5a8ae374c3cd767d0bfa354524ddd5f03c898d3a
parksital <rsital@icloud.com>
2020-03-18 12:40:55 +0100

    refactored test

--- AtlasTests/EventListInteractorTests.swift
+++ AtlasTests/EventListInteractorTests.swift
@@ -8,58 +8,6 @@
 
 import XCTest
 class EventListInteractorTests: XCTestCase {
-    private class MockEventService: EventService {
-        var shouldPass: Bool = true
-        
-        init(shouldPass: Bool) {
-            self.shouldPass = shouldPass
-            super.init(client: nil)
-        }
-        
-        override func fetchEventsSummarized(_ completion: @escaping (Result<[EventSummary], Error>) -> Void) {
-            
-            if shouldPass {
-                let data = MockEventService.getFakeSummaries(count: 5)
-                completion(.success(data))
-            } else {
-                completion(.failure(NetworkError.noEvents))
-            }
-        }
-        
-        static func getFakeSummaries(count: Int) -> [EventSummary] {
-            var result: [EventSummary] = []
-            for i in 1...count {
-                let summary = EventSummary(
-                    id: String(i),
-                    title: "Mock title \(i)",
-                    startDate: Date(timeIntervalSince1970: TimeInterval(i)),
-                    venue: "Venue name \(i)"
-                )
-                
-                result.append(summary)
-            }
-            return result
-        }
-    }
-    
-    private class PresenterSpy: EventListPresentationLogic {
-        var errorReceived: Error?
-        var eventsReceived: [EventSummary] = []
-        var eventSelected: EventSummary?
-        
-        func presentEvents(_ events: [EventSummary]) {
-            eventsReceived = events
-        }
-        
-        func didSelectEvent(_ event: EventSummary) {
-            eventSelected = event
-        }
-        
-        func presentError(_ error: Error) {
-            errorReceived = error
-        }
-    }
-    
     var sut: EventListInteractor!
     
     override func setUp() {
@@ -126,5 +74,60 @@ class EventListInteractorTests: XCTestCase {
         let expectation = events[2]
         let result = spy.eventSelected
         XCTAssertEqual(result, expectation)
+    }
+}
+
+//MARK: - Helpers
+extension EventListInteractorTests {
+    private class MockEventService: EventService {
+        var shouldPass: Bool = true
+        
+        init(shouldPass: Bool) {
+            self.shouldPass = shouldPass
+            super.init(client: nil)
+        }
+        
+        override func fetchEventsSummarized(_ completion: @escaping (Result<[EventSummary], Error>) -> Void) {
+            
+            if shouldPass {
+                let data = MockEventService.getFakeSummaries(count: 5)
+                completion(.success(data))
+            } else {
+                completion(.failure(NetworkError.noEvents))
+            }
+        }
+        
+        static func getFakeSummaries(count: Int) -> [EventSummary] {
+            var result: [EventSummary] = []
+            for i in 1...count {
+                let summary = EventSummary(
+                    id: String(i),
+                    title: "Mock title \(i)",
+                    startDate: Date(timeIntervalSince1970: TimeInterval(i)),
+                    venue: "Venue name \(i)"
+                )
+                
+                result.append(summary)
+            }
+            return result
+        }
+    }
+    
+    private class PresenterSpy: EventListPresentationLogic {
+        var errorReceived: Error?
+        var eventsReceived: [EventSummary] = []
+        var eventSelected: EventSummary?
+        
+        func presentEvents(_ events: [EventSummary]) {
+            eventsReceived = events
+        }
+        
+        func didSelectEvent(_ event: EventSummary) {
+            eventSelected = event
+        }
+        
+        func presentError(_ error: Error) {
+            errorReceived = error
+        }
     }
 }
