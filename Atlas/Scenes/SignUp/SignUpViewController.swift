99310ccf8db0042e5fd3af4f7b45963ffe544723
parksital <rsital@icloud.com>
2020-04-23 05:58:07 +0200

    created signupviewcontroller file and protocol

---
+++ Atlas/Scenes/SignUp/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Atlas
+//
+//  Created by Parvin Sital on 23/04/2020.
+//  Copyright © 2020 Parvin Sital. All rights reserved.
+//
+
+import UIKit
+
+protocol SignUpDisplayLogic: class {
+    
+}
+
+final class SignUpViewController {
+    
+}
