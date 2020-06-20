ea2d0429af59d49d04a23da15f3676dc8588f776
parksital <rsital@icloud.com>
2020-06-20 16:56:18 +0200

    eventdetail dependencies

--- Atlas/SceneContainer.swift
+++ Atlas/SceneContainer.swift
@@ -29,25 +29,7 @@ class SceneContainer {
         EventListDI.setDependencies(inContainer: container)
         
         //MARK: - EventDetail Dependencies
-        container.autoregister(EventDetailInteraction.self, initializer: EventDetailInteractor.init)
-        container.autoregister(EventDetailPresentationLogic.self, initializer: EventDetailPresenter.init)
-        container.autoregister(EventDetailRouterProtocol.self, initializer: EventDetailRouter.init)
-        container.register(EventDetailViewController.self) { r in
-            EventDetailViewController()
-        }.initCompleted { r, vc in
-            let interactor = r.resolve(EventDetailInteraction.self)!
-            let router = r.resolve(EventDetailRouterProtocol.self)!
-            let presenter = r.resolve(EventDetailPresentationLogic.self)!
-            
-            vc.setup(interactor: interactor)
-            vc.setup(router: router)
-
-            
-            router.setup(dataStore: interactor)
-            router.setup(viewController: vc)
-            
-            presenter.setup(viewController: vc)
-        }
+        EventDetailDI.setDependencies(inContainer: container)
         
         //MARK: - Account Depenedencies
         container.autoregister(AccountInteraction.self, initializer: AccountInteractor.init)
