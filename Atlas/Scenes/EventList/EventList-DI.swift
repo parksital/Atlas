151e423dd35641e7dd02c2c1522602947bb06b95
parksital <rsital@icloud.com>
2020-06-20 16:36:43 +0200

    remember to save files before commiting

--- Atlas/Scenes/EventList/EventList-DI.swift
+++ Atlas/Scenes/EventList/EventList-DI.swift
@@ -10,7 +10,7 @@ import Foundation
 import Swinject
 
 class EventListDI {
-    static func setDependencies(inContaiener container: Container) {
+    static func setDependencies(inContainer container: Container) {
         container.autoregister(EventListInteraction.self, initializer: EventListInteractor.init)
         container.autoregister(EventListPresentationLogic.self, initializer: EventListPresenter.init)
         container.autoregister(EventListRouterProtocol.self, initializer: EventListRouter.init)
