42b41c4fd053616bd651bdb1170eb6810def33b2
parksital <rsital@icloud.com>
2020-07-08 19:28:21 +0200

    wipe keychain button height: 44

--- Atlas/Scenes/Account/Preferences/PreferencesViewController.swift
+++ Atlas/Scenes/Account/Preferences/PreferencesViewController.swift
@@ -8,6 +8,61 @@
 
 import UIKit
 
+final class WipeKeychainCell: UITableViewCell {
+    private let button = UIButton(type: .system)
+    var action: (() -> Void)?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension WipeKeychainCell {
+    func setupViews() {
+        self.selectionStyle = .none
+        setupButton()
+    }
+    
+    func setupButton() {
+        button.setTitle("Wipe Keychain", for: .normal)
+        button.tintColor = .systemRed
+        button.addTarget(
+            self,
+            action: #selector(self.buttonTapped),
+            for: .touchUpInside
+        )
+        
+        setupButtonConstraints()
+    }
+    
+    func setupButtonConstraints() {
+        contentView.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leading = button.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor)
+        let top = button.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor)
+        let bottom = button.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor)
+        let height = button.heightAnchor.constraint(equalToConstant: 44.0)
+        
+        NSLayoutConstraint.activate([leading, top, bottom, height])
+    }
+    
+    @objc func buttonTapped() {
+        action?()
+    }
+}
+
+extension WipeKeychainCell {
+    func configure() {
+        
+    }
+}
+
 enum PreferenceSectionType: Int, CaseIterable {
     case preferences
 }
@@ -50,7 +105,6 @@ private extension PreferencesViewController {
         view.backgroundColor = .systemBackground
         setupNavigation()
         setupTableView()
-        setupWipeKeychainButton()
     }
     
     func setupNavigation() {
@@ -58,9 +112,14 @@ private extension PreferencesViewController {
     }
     
     func setupTableView() {
+        registerTableViewViewCells()
         setupTableViewConstraints()
         configureTableViewDatasource()
         setInitialSnapshot()
+    }
+    
+    func registerTableViewViewCells() {
+        tableView.register(cellType: WipeKeychainCell.self)
     }
     
     func setupTableViewConstraints() {
@@ -79,8 +138,9 @@ private extension PreferencesViewController {
         dataSource = DataSource(tableView: tableView) { (tableView, indexPath, item) -> UITableViewCell? in
             switch item {
             case .wipeKeychain:
-                let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "")
-                
+                let cell: WipeKeychainCell = tableView.getCell(forIndexPath: indexPath)
+                cell.action = { print("wipe keychain") }
+                cell.configure()
                 return cell
             }
         }
@@ -89,19 +149,8 @@ private extension PreferencesViewController {
     func setInitialSnapshot() {
         currentSnapshot = Snapshot()
         currentSnapshot?.appendSections(PreferenceSectionType.allCases)
+        currentSnapshot?.appendItems([.wipeKeychain], toSection: .preferences)
         dataSource?.apply(currentSnapshot!, animatingDifferences: false)
-    }
-    
-    func setupWipeKeychainButton() {
-        wipeKeychainButton.setTitle("Wipe Keychain", for: .normal)
-        wipeKeychainButton.tintColor = .systemRed
-        wipeKeychainButton.addTarget(
-            self,
-            action: #selector(self.wipeKeychain),
-            for: .touchUpInside
-        )
-        
-        stackView.addArrangedSubview(wipeKeychainButton)
     }
     
     @objc func wipeKeychain() {
